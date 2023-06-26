9d3362451812416dcbc56003249e0bbc6f66b9e6
Raul Neiva <raulneivaweb@gmail.com>
2023-06-25 21:13:17 -0300

    update

--- pre-flight.sh
+++ pre-flight.sh
@@ -7,9 +7,10 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+RED='\033[0;31m'
 GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
 NC='\033[0m'
-DOCKER_BOX_PATH="$HOME/localcloud"
 
 function log() {
   echo
@@ -17,24 +18,41 @@ function log() {
   echo
 }
 
-log "Installing git..."
+function log_success() {
+  echo
+  echo -e "✅ ${GREEN}${1}${NC}"
+  echo
+}
 
+function log_warn() {
+  echo
+  echo -e "⚠️ ${YELLOW}${1}${NC}"
+  echo
+}
+
+function log_error() {
+  echo
+  echo -e "⚠️ ${RED}ERROR: ${1}${NC}"
+  echo
+}
+
+log_success "Installing git..."
 apt-get update -qq
 apt-get -qqy install git
 
 if [ ! -d "$DOCKER_BOX_PATH" ]; then
-  log "Downloading localcloud..."
+  log_warn "Downloading localcloud..."
   git clone \
     --depth 1 \
     https://github.com/rneiva/localcloud \
     "$DOCKER_BOX_PATH"
 fi
 
-log "Updating localcloud..."
+log_warn "Updating localcloud..."
 cd "$DOCKER_BOX_PATH"
 git pull
 
-log "Setting permissions to folder"
+log_warn "Setting permissions to folder"
 chown ubuntu:ubuntu -R "$DOCKER_BOX_PATH"
 chmod +x *.sh
 
